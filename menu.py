8e1d35031614f0cfad35bad3e187d5091dd7b0f6
domenicapelaez <domenicapelaez3@gmail.com>
2020-12-15 22:43:01 +0100

    practica

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import hashlib
 import os
-
+#mis funciones
 def convertirMD5():
 
     print(hashlib.md5(cadena.encode('utf-8')).hexdigest())
@@ -8,7 +8,7 @@ def convertirMD5():
 def convertirFichMD5():
     with open(cadena, "rb") as f:
         print(hashlib.md5(f.read()).hexdigest())
-
+    #rb se involucra en la lectura del fichero
 def convertirSHA1():
     print(hashlib.sha1(cadena.encode('utf-8')).hexdigest())
 
@@ -28,16 +28,17 @@ def menu():
 
     return num
 
+#\n salto de linea para la ejecución, por estetica
 
 salir = False
 opcion = 0
-
+#se muestra el menu, el while es un bucle que no termina hasta que pulsamos "5. Salir"
 while not salir:
 
-    print("1. Resumen de MD5 de una cadena")
-    print("2. Resumen de MD5 de un fichero")
-    print("3. Resumen de SHA1 de una cadena")
-    print("4. Resumen de SHA1 de un fichero")
+    print("1. Resumen MD5 en una cadena")
+    print("2. Resumen MD5 en un fichero")
+    print("3. Resumen SHA1 en una cadena")
+    print("4. Resumen SHA1 en un fichero")
     print("5. Salir \n")
 
     print("Elige una opcion")
@@ -45,29 +46,30 @@ while not salir:
     opcion = menu()
 
     if opcion == 1:
-        print("Resumen de MD5 de una cadena: \n")
+        print("Resumen MD5 en una cadena: \n")
         cadena=str(input("Introduce la cadena para realizar el resumen MD5: \n"))
         print("\n")
         convertirMD5()
         print("\n")
     elif opcion == 2:
-        print("Resumen de MD5 de un fichero: \n")
-        valido = False
+        print("Resumen MD5 en un fichero: \n")
+        valido = False #bucles, se ejecuta mientras sea falso, cuando detecta el archivo, lo convierte en true
         while valido == False:
-            cadena=str(input("Introduce el fichero para obtener el resumen MD5: \n"))
+            cadena=str(input("Introduce el fichero para obtener el resumen MD5: \n")) #se encarga de añadir el fichero
+
             if os.path.isfile(cadena):
                 valido = True
                 convertirFichMD5()
                 print("\n")
             else:
-                print("El archivo especificado no existe \n")
+                print("El archivo no existe \n")
     elif opcion == 3:
-        print("Resumen de SHA1 de una cadena: \n")
+        print("Resumen SHA1 en una cadena: \n")
         cadena=str(input("Introduce la cadena para realizar el resumen SHA1: \n"))
         convertirSHA1()
         print("\n")
     elif opcion == 4:
-        print("Resumen de SHA1 de un fichero: \n")
+        print("Resumen SHA1 en un fichero: \n")
         valido2 = False
         while valido2 == False:
             cadena = str(input("Introduce el fichero para obtener el resumen SHA1 \n"))
@@ -75,11 +77,12 @@ while not salir:
                 valido2 = True
                 convertirFichSHA1()
                 print("\n")
+            #este if es para poder atacar el contenido del fichero
             else:
-                print("El archivo especificado no existe \n")
+                print("El archivo no existe \n")
     elif opcion == 5:
         salir = True
     else:
-        print("Introduce un numero entre 1 y 4 \n")
-
-print("\nSaliendo del menú...")
+        print("Introduce un numero entre 1 y 5 \n")
+    #si se indica un numero diferente entre 1-5, se hace un reset y nos indica la opcion valida
+print("\nHas salido del menú!")
